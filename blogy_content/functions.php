710f244a1e5ef54a0720385e430f43d1b8b7cde3
arguiot <arguiot@gmail.com>
2016-09-06 16:13:10 +1000

    Update functions.php

--- blogy_content/functions.php
+++ blogy_content/functions.php
@@ -5,3 +5,19 @@ function input($match, $regex)
   $file_requested = file_get_contents(substr($match, 5, -1));
   $page = preg_replace($regex, $file_requested, $page);
 }
+
+
+function foreach($match, $regex)
+{
+  $foreach_content = substr($match, 15, -8);
+  $sql = "SELECT * FROM $table ORDER BY $table_post_date DESC lIMIT 0, $article_number";
+  foreach($bdd->query($sql) as $val)
+{
+  $articleTitle = $val['post_title'];
+  $articleID = $val['ID'];
+  $articleDate = $val['post_date'];
+  $articleDesc = strip_tags(mb_strimwidth($val['post_content'], 0, $descSize, "..."));
+  echo articleTagsReplace($foreach_content);
+}
+  $page = preg_replace($regex, $result, $page);
+}
